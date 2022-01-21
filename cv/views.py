b0682a5659d1e68300192fceb942b4b37bd99317
Adesegun Adesola ADEBOYE <fantasticinkmedia@gmail.com>
2022-01-21 18:54:11 +0100

    Created view request

--- cv/views.py
+++ cv/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is an application about resume building")
+    return render(request, "oyebola.html")
 
